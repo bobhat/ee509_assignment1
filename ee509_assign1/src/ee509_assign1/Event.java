c84a7b9846db15fbef6788fd5f9ade148c6feddb
bobha <bobha@DESKTOP-L9J6TUG>
2016-11-04 13:06:43 +0000

    New Class files

---
+++ ee509_assign1/src/ee509_assign1/Event.java
@@ -0,0 +1,42 @@
+package ee509_assign1;
+
+public class Event {
+	
+	/**
+	 * The clock value of the event.
+	 */
+	private double eventClock;
+
+	/**
+	 * Constructor of the class: to be developed by the student...
+	 * 
+	 */
+	public Event(double eventClock) { 
+		
+		// Fill in with your code....
+	}
+
+	/**
+	 * Returns the event clock value.
+	 * 
+	 * @return (double) eventClock : the even clock value (double).
+	 */
+	public double getEventClock() {
+		return eventClock;
+	}
+
+	/**
+	 * Modifies the event clock value.
+	 * 
+	 * @param (double) eventClock
+	 *            : the new event clock value (double).
+	 */
+	public void setEventClock(double eventClock) {
+		this.eventClock = eventClock;
+	}
+	
+	
+	//Add attributes that are necessary to uniquely define the event object.
+	// ....
+
+}
